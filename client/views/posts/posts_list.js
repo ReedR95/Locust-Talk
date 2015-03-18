18cd7ac8f0a5cdc246b75e893f38ea3b4e9ca307
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2015-03-18 14:17:33 -0400

    post list

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,26 @@
+var postsData = [
+  {
+    author: 'Reed Rosenbluth',
+    content: 'Ugh McSweeney\'s Truffaut Neutra hella. Normcore single-origin coffee church-key, mumblecore gluten-free biodiesel Neutra banjo hashtag.'
+  },
+  {
+    author: 'Jon Dubin',
+    content: 'Ugh McSweeney\'s Truffaut Neutra hella. Normcore single-origin coffee church-key, mumblecore gluten-free biodiesel Neutra banjo hashtag.'
+  },
+  {
+    author: 'Derek Jobst',
+    content: 'Ugh McSweeney\'s Truffaut Neutra hella. Normcore single-origin coffee church-key, mumblecore gluten-free biodiesel Neutra banjo hashtag.'
+  },
+  {
+    author: 'Satya Bodduluri',
+    content: 'Ugh McSweeney\'s Truffaut Neutra hella. Normcore single-origin coffee church-key, mumblecore gluten-free biodiesel Neutra banjo hashtag.'
+  },
+  {
+    author: 'Sacha Best',
+    content: 'Ugh McSweeney\'s Truffaut Neutra hella. Normcore single-origin coffee church-key, mumblecore gluten-free biodiesel Neutra banjo hashtag.'
+  },
+]
+
+Template.feed.helpers({
+  posts: postsData
+});
