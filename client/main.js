13f77d99fa4717541c1433189649b940e9549585
Jonathan Dubin <mail@jondubin.com>
2015-03-19 11:07:11 -0400

    Iron router.

--- client/main.js
+++ client/main.js
@@ -1,30 +1,36 @@
 //if (Meteor.isClient) {
-  
-  if (!Session.get('identity')) {
-    Session.set('identity', Date.now());
-  }
 
-  Template.compose.events({
-    'click button': function (e) {
-      var $content = $(e.target).parent().find('textarea');
-      
-      post = {
-        date_created: Date.now(),
-        author: Session.get('identity'),
-        content: $content.val()
-      }
-      if (post.content) {
-        Posts.insert(post);
-        $content.val('');
-      } else {
-        alert('your post must not be empty...');
-      }
-    }
-  });
+Router.route('/', function(){
+  //this.render('main');
   
-  Template.compose.helpers({
-    identity: function() {
-      return Session.get('identity');
-    }
-  });
+  // if (!Session.get('identity')) {
+  //   Session.set('identity', Date.now());
+  // }
+  // 
+  // Template.compose.events({
+  //   'click button': function (e) {
+  //     var $content = $(e.target).parent().find('textarea');
+  //     
+  //     post = {
+  //       date_created: Date.now(),
+  //       author: Session.get('identity'),
+  //       content: $content.val()
+  //     }
+  //     if (post.content) {
+  //       Posts.insert(post);
+  //       $content.val('');
+  //     } else {
+  //       alert('your post must not be empty...');
+  //     }
+  //   }
+  // });
+  // 
+  // Template.compose.helpers({
+  //   identity: function() {
+  //     return Session.get('identity');
+  //   }
+  // });
+})
+  
+
 //}
